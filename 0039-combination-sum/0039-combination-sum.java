20469e029a6c2f49eaa3458e3e2964424673b62e
ankitbhardwaj7488-afk <ankitbhardwaj7488@gmail.com>
2026-07-14 11:26:23 +0530

    Time: 3 ms (35.08%), Space: 45.6 MB (83.08%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,50 +1,35 @@
 class Solution {
- List<List<Integer>> result = new ArrayList<>();
 
+    List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-        backtrack(candidates, target, 0, new ArrayList<>());
-
+        backtrack(candidates, target , 0, new ArrayList<>());
         return result;
+        
     }
 
-    public void backtrack(int[] candidates, int target, int index, List<Integer> current) {
-
-        // Base Case 1: Found a valid combination
-        if (target == 0) {
+    public void backtrack(int[] candidates, int target ,int index, List<Integer> current)
+    {
+        //Base case 1:found a valid combination 
+        if(target == 0){
             result.add(new ArrayList<>(current));
             return;
         }
 
-        // Base Case 2: Invalid path
-        if (target < 0) {
+        //Base case 2 : invalid path
+        if(target <0){
             return;
         }
 
-        // Base Case 3: No more candidates
-        if (index == candidates.length) {
+        //Base case 3 : no more candidates
+        if(index ==candidates.length ){
             return;
         }
 
-        // ----------------------------
-        // Choice 1 : Take current number
-        // ----------------------------
+        //Choice 1 : Take current number 
         current.add(candidates[index]);
+        backtrack(candidates,target- candidates[index], index ,current );
+        current. remove(current.size()-1);
 
-        backtrack(candidates,
-                  target - candidates[index],
-                  index,
-                  current);
-
-        // Backtrack
-        current.remove(current.size() - 1);
-
-        // ----------------------------
-        // Choice 2 : Skip current number
-        // ----------------------------
-        backtrack(candidates,
-                  target,
-                  index + 1,
-                  current);
+        backtrack(candidates,target, index +1, current );
     }
-    }
+}
